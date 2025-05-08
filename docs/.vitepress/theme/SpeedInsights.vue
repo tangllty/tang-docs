a7698b8c4bafa1b66a7a52410f8e723c569a449f
Tang <tangllty@163.com>
2025-05-08 17:07:23 +0800

    Fix Vercel Speed Insights didn't work

---
+++ docs/.vitepress/theme/SpeedInsights.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { SpeedInsights } from '@vercel/speed-insights/vue'
+import { Content } from 'vitepress'
+</script>
+
+<template>
+  <SpeedInsights />
+  <Content />
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,17 @@
+import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
+import SpeedInsights from './SpeedInsights.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     enhanceAppWithTabs(app)
+  },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'aside-outline-before': () => h(SpeedInsights)
+    })
   }
 } satisfies Theme
