2c44a6ba4c3cbb9e0b507d2ff62d8a0a83e99223
Tang <tangllty@163.com>
2025-06-11 15:56:16 +0800

    Remove unused Content component from SpeedInsights.vue

--- docs/.vitepress/theme/SpeedInsights.vue
+++ docs/.vitepress/theme/SpeedInsights.vue
@@ -1,9 +1,7 @@
 <script setup>
 import { SpeedInsights } from '@vercel/speed-insights/vue'
-import { Content } from 'vitepress'
 </script>
 
 <template>
   <SpeedInsights />
-  <Content />
 </template>
